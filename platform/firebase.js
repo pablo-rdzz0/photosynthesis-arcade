db39fda47354fdc52508aa19b02ed2fe468dee5b
pablo-rdzz0 <pablo.alejandro.rdz.29@gmail.com>
2025-11-09 13:47:39 -0600

    Update firebase.js

--- platform/firebase.js
+++ platform/firebase.js
@@ -1,4 +1,3 @@
-<script type="module">
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-app.js";
 import {
   getAuth,
@@ -95,4 +94,3 @@ window.firebaseStuff = {
     });
   },
 };
-</script>
